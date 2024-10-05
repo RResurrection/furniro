df1848e0225cdea86a9b18f1e0a282b35efbc4cd
myroslavgrekul@gmail.com <myroslavgrekul@gmail.com>
2024-10-05 16:51:18 +0300

    add gallery

--- src/components/Home/slider/slider.tsx
+++ src/components/Home/slider/slider.tsx
@@ -7,7 +7,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import { slider } from "../../../utils/mock/slider/slider.json";
+import { slider } from "@/utils/mock/slider/slider.json";
 
 const Slider = () => {
   return (
